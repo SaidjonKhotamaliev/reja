e63f10e0ffea7c5a5511c42eec8c015254d500e1
SaidjonKhotamaliev <saidjon.khotamaliev@gmail.com>
2024-05-15 18:29:08 +0900

    master

--- server.js
+++ server.js
@@ -14,15 +14,22 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4:Routing code
-app.get("/", function (req, res) {
-  res.end("<h1>Hello World!</h1>");
+// app.get("/", function (req, res) {
+//   res.end("<h1>Hello World!</h1>");
+// });
+// app.get("/gift", function (req, res) {
+//   res.end("<h1>Siz sovg'alar bo'limidasiz</h1>");
+// });
+app.post("/create-item", (req, res) => {
+  console.log(req.body);
+  res.json({ test: "success" });
 });
-app.get("/gift", function (req, res) {
-  res.end("<h1>Siz sovg'alar bo'limidasiz</h1>");
+app.get("/", function (req, res) {
+  res.render("harid");
 });
 
 const server = http.createServer(app);
-let PORT = 3001;
+let PORT = 3000;
 server.listen(PORT, function () {
   console.log("The server is running succesfully on port: " + PORT);
 });
