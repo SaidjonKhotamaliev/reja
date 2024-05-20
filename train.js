c41b53552438535d1816303a3728ace9b365f1d5
SaidjonKhotamaliev <saidjon.khotamaliev@gmail.com>
2024-05-20 19:52:06 +0900

    feat: A TASK problem has been solved

--- train.js
+++ train.js
@@ -1,37 +1,55 @@
-console.log("Jack Ma maslahatlari");
-const list = [
-  "yaxshi talaba bo'ling", // 0-20
-  "to'g'ri boshliq tanlang va ko'proq xato qiling", // 20-30
-  "uzingizga ishlashni boshlang", // 30-40
-  "siz kuchli bo'lgan narsalarni qiling", // 40-50
-  "yoshlarga investitsiya qiling", // 50-60
-  "endi dam oling, foydasi yo'q", // 60+
-];
+// A TASK:
+//Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi parametrdagi sozdan qatnashga sonini return qilishi kerak boladi.
+// MASALAN countLetter("e", "engineer") 3ni return qiladi.
 
-// Callback function define part
-
-function maslahatBering(a, callback) {
-  if (typeof a !== "number") callback("insert a number", null);
-  else if (a <= 20) callback(null, list[0]);
-  else if (a > 20 && a <= 30) callback(null, list[1]);
-  else if (a > 30 && a <= 40) callback(null, list[2]);
-  else if (a > 40 && a <= 50) callback(null, list[3]);
-  else if (a > 50 && a <= 60) callback(null, list[4]);
-  else {
-    setInterval(function () {
-      callback(null, list[5]);
-    }, 2000);
-  }
+function justFunction(letter, word, callback) {
+  if (typeof letter !== "string" && typeof word !== "string")
+    callback("insert a string!", null);
+  else if (word.includes(letter)) {
+    let javob = word.split(letter).length - 1;
+    callback(null, javob);
+  } else callback(null, 0);
 }
 
-// call callback function
-
-console.log("passed here 0");
-maslahatBering(65, (err, data) => {
+justFunction("e", "engineer", (err, data) => {
   if (err) console.log("ERROR: ", err);
   console.log("javob:", data);
 });
-console.log("passed here 1");
+
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//   "yaxshi talaba bo'ling", // 0-20
+//   "to'g'ri boshliq tanlang va ko'proq xato qiling", // 20-30
+//   "uzingizga ishlashni boshlang", // 30-40
+//   "siz kuchli bo'lgan narsalarni qiling", // 40-50
+//   "yoshlarga investitsiya qiling", // 50-60
+//   "endi dam oling, foydasi yo'q", // 60+
+// ];
+
+// Callback function define part
+
+// function maslahatBering(a, callback) {
+//   if (typeof a !== "number") callback("insert a number", null);
+//   else if (a <= 20) callback(null, list[0]);
+//   else if (a > 20 && a <= 30) callback(null, list[1]);
+//   else if (a > 30 && a <= 40) callback(null, list[2]);
+//   else if (a > 40 && a <= 50) callback(null, list[3]);
+//   else if (a > 50 && a <= 60) callback(null, list[4]);
+//   else {
+//     setInterval(function () {
+//       callback(null, list[5]);
+//     }, 2000);
+//   }
+// }
+
+// call callback function
+
+// console.log("passed here 0");
+// maslahatBering(65, (err, data) => {
+//   if (err) console.log("ERROR: ", err);
+//   console.log("javob:", data);
+// });
+// console.log("passed here 1");
 
 // async fujnction
 
