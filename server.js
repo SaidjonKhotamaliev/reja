8f20aefad7257318b24498cf1846b4ff3934e640
SaidjonKhotamaliev <saidjon.khotamaliev@gmail.com>
2024-05-23 23:21:47 +0900

    feat: mongoDB ulanish logic

--- server.js
+++ server.js
@@ -1,52 +1,26 @@
-console.log("Web serverni boshlash");
-const express = require("express");
-const app = express();
 const http = require("http");
-const fs = require("fs");
+const mongodb = require("mongodb");
+let db;
+const connectionString =
+  "mongodb+srv://saidjonkhotamaliev:Saidjon123@cluster0.rb0elgd.mongodb.net/Reja";
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-  if (err) {
-    console.log("ERROR: ", err);
-  } else {
-    user = JSON.parse(data);
+mongodb.connect(
+  connectionString,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err, client) => {
+    if (err) console.log("ERROR on connection MongoDB ", err);
+    else {
+      console.log("MongoDB connection succeed");
+      module.exports = client;
+      const app = require("./app");
+      const server = http.createServer(app);
+      let PORT = 3000;
+      server.listen(PORT, function () {
+        console.log(
+          "The server is running succesfully on port: " + PORT,
+          "http://localhost:" + PORT
+        );
+      });
+    }
   }
-});
-
-// 1: Kirish code
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// 2: Session code
-// 3: Views code
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-// 4:Routing code
-// app.get("/", function (req, res) {
-//   res.end("<h1>Hello World!</h1>");
-// });
-// app.get("/gift", function (req, res) {
-//   res.end("<h1>Siz sovg'alar bo'limidasiz</h1>");
-// });
-app.post("/create-item", (req, res) => {
-  //   console.log(req.body);
-  //   res.json({ test: "success" });
-});
-
-app.get("/author", (req, res) => {
-  res.render("author", { user: user });
-});
-
-app.get("/", function (req, res) {
-  res.render("reja");
-});
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
-  console.log(
-    "The server is running succesfully on port: " + PORT,
-    "http://localhost:" + PORT
-  );
-});
+);
