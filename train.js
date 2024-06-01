8f520318cf51625659cdced2569714e959142e3a
SaidjonKhotamaliev <saidjon.khotamaliev@gmail.com>
2024-06-02 05:24:18 +0900

    feat: TASK F was done here

--- train.js
+++ train.js
@@ -4,61 +4,66 @@ Shunday findDoublers function tuzing, unga faqat bitta string argument pass boli
 MASALAN: getReverse("hello") return true return qiladi
 */
 
-function getReverse(string) {
-  let new_array = [];
-  for (let i = string.length - 1; i >= 0; i--) {
-    new_array.push(string[i]);
-  }
-  const new_string = new_array.join("");
-  return new_string;
-}
-console.log(getReverse("hello"));
-
-// Explanation => Animal chanllenge
-const animal_list = [
-  "fox",
-  "ant",
-  "bird",
-  "lion",
-  "deer",
-  "wolf",
-  "bear",
-  "from",
-  "hen",
-  "mole",
-  "duck",
-  "dog",
-  "goat",
-  "bat",
-  "cock",
-  "cat",
-  "cow",
-];
-
-let natija = [];
-
-function findAnimals(txt, animal_list) {
-  for (let i = 0; i < animal_list.length; i++) {
-    let animal = animal_list[i];
-    let count = 0;
-
-    for (let j = 0; j < animal.length; j++) {
-      let animalLetter = animal[j];
-
-      for (let k = 0; k < txt.length; k++) {
-        if (animalLetter === txt[k]) {
-          count++;
-          break;
-        }
+function getSameLetter(a) {
+  let natija = false;
+  for (let i = 0; i < a.length; i++) {
+    for (let j = i + 1; j < a.length; j++) {
+      if (a[i] === a[j]) {
+        natija = true;
+        break;
       }
     }
-
-    if (count == animal.length) {
-      natija.push(animal);
-    }
   }
-  return result;
+  return natija;
 }
 
-const javob = findAnimals("dgota", animal_list);
-console.log("javob:", javob);
+console.log(getSameLetter("hello"));
+
+// Explanation => Animal chanllenge
+// const animal_list = [
+//   "fox",
+//   "ant",
+//   "bird",
+//   "lion",
+//   "deer",
+//   "wolf",
+//   "bear",
+//   "from",
+//   "hen",
+//   "mole",
+//   "duck",
+//   "dog",
+//   "goat",
+//   "bat",
+//   "cock",
+//   "cat",
+//   "cow",
+// ];
+
+// let natija = [];
+
+// function findAnimals(txt, animal_list) {
+//   for (let i = 0; i < animal_list.length; i++) {
+//     let animal = animal_list[i];
+//     let count = 0;
+
+//     for (let j = 0; j < animal.length; j++) {
+//       let animalLetter = animal[j];
+
+//       for (let k = 0; k < txt.length; k++) {
+//         if (animalLetter === txt[k]) {
+//           count++;
+//           break;
+//         }
+//       }
+//     }
+
+//     if (count == animal.length) {
+//       natija.push(animal);
+//     }
+//   }
+//   return result;
+// }
+
+// const javob = findAnimals("dgota", animal_list);
+// console.log("javob:", javob);
