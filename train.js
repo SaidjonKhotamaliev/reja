1d26a73b526ac52d26002d8cbfa45a6b945463e7
SaidjonKhotamaliev <saidjon.khotamaliev@gmail.com>
2024-06-01 19:43:43 +0900

    feat: TASK F was solved here

--- train.js
+++ train.js
@@ -1,57 +1,70 @@
-// E-TASK:
+/*F-TASK: 
 
-// Shunday function tuzing, u bitta string argumentni qabul qilib osha stringni teskari qilib return qilsin.
-// MASALAN: getReverse("hello") return qilsin "olleh"
+Shunday findDoublers function tuzing, unga faqat bitta string argument pass bolib, agar stringda bir hil harf qatnashgan bolsa true, qatnashmasa false qaytarishi kerak.
+MASALAN: getReverse("hello") return true return qiladi
+*/
 
-function getReverse(string) {
-  return string.split("").reverse().join("");
-}
-console.log(getReverse("yowhatsupmf"));
-
-// Explanation => Animal chanllenge
-const animal_list = [
-  "fox",
-  "ant",
-  "bird",
-  "lion",
-  "deer",
-  "wolf",
-  "bear",
-  "from",
-  "hen",
-  "mole",
-  "duck",
-  "dog",
-  "goat",
-  "bat",
-  "cock",
-  "cat",
-  "cow",
-];
-
-let natija = [];
-
-function findAnimals(txt, animal_list) {
-  for (let i = 0; i < animal_list.length; i++) {
-    let animal = animal_list[i];
-    let count = 0;
-    for (let j = 0; j < animal.length; j++) {
-      let animalLetter = animal[j];
-      for (let k = 0; k < txt.length; k++) {
-        if (animalLetter === txt[k]) {
-          count++;
-          break;
-        }
+function getSameLetter(a) {
+  let result = false;
+  debugger;
+  for (let i = 0; i < a.length; i++) {
+    const element = a[i];
+    for (let j = i + 1; j < a.length; j++) {
+      if (element == a[j]) {
+        result = true;
+        break;
       }
     }
-    if (count == animal.length) {
-      natija.push(animal);
-    }
   }
-  return natija;
+  return result;
 }
 
-const javob = findAnimals("dgota", animal_list);
-console.log("javob:", javob);
+console.log(getSameLetter("hakunamatata"));
 
-// Explanation Sezar Challenge
+// Explanation => Animal chanllenge
+// const animal_list = [
+//   "fox",
+//   "ant",
+//   "bird",
+//   "lion",
+//   "deer",
+//   "wolf",
+//   "bear",
+//   "from",
+//   "hen",
+//   "mole",
+//   "duck",
+//   "dog",
+//   "goat",
+//   "bat",
+//   "cock",
+//   "cat",
+//   "cow",
+// ];
+
+// let natija = [];
+
+// function findAnimals(txt, animal_list) {
+//   for (let i = 0; i < animal_list.length; i++) {
+//     let animal = animal_list[i];
+//     let count = 0;
+//     for (let j = 0; j < animal.length; j++) {
+//       let animalLetter = animal[j];
+//       for (let k = 0; k < txt.length; k++) {
+//         if (animalLetter === txt[k]) {
+//           count++;
+//           break;
+//         }
+//       }
+//     }
+//     if (count == animal.length) {
+//       natija.push(animal);
+//     }
+//   }
+//   return natija;
+// }
+
+// const javob = findAnimals("dgota", animal_list);
+// console.log("javob:", javob);
+
+// // Explanation Sezar Challenge
